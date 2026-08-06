7c9fa149a2a8b6bf8fea59224403cc69dc99ae12
Kim Erik Reinhold <dalereinhold@outlook.com>
2026-08-06 19:58:40 +0200

    Updated empty-state.tsx

--- src/components/empty-state.tsx
+++ src/components/empty-state.tsx
@@ -1,7 +1,7 @@
 import type { LucideIcon } from "lucide-react"
 import { Plus } from "lucide-react"
 
-import { Button } from "@/components/ui/Button"
+import { Button } from "@/components/ui/button"
 import {
   Card,
   CardContent,
